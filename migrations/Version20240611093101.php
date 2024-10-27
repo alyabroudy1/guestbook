82216675bbacb186d470754905264ff7918ac8da
mohammad-al-yabroudy <alyabroudy1@gmail.com>
2024-10-27 09:44:16 +0100

    adapting symfony-docker project

--- migrations/Version20240611093101.php
+++ migrations/Version20240611093101.php
@@ -38,18 +38,18 @@ final class Version20240611093101 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_DABA824C12469DE2 ON movie_category (category_id)');
         $this->addSql('CREATE TABLE server (id INT NOT NULL, name VARCHAR(255) DEFAULT NULL, headers JSON DEFAULT NULL, cookie TEXT DEFAULT NULL, authority VARCHAR(255) DEFAULT NULL, rate SMALLINT DEFAULT NULL, active BOOLEAN DEFAULT NULL, default_authority VARCHAR(255) DEFAULT NULL, model VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_5A6DD5F6D79572D9 ON server (model)');
-//        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-//        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
-//        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
-//        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
-//        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
-//            BEGIN
-//                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
-//                RETURN NEW;
-//            END;
-//        $$ LANGUAGE plpgsql;');
-//        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
-//        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
         $this->addSql('ALTER TABLE link ADD CONSTRAINT FK_36AC99F11844E6B7 FOREIGN KEY (server_id) REFERENCES server (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE link ADD CONSTRAINT FK_36AC99F18F93B6FC FOREIGN KEY (movie_id) REFERENCES movie (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE movie ADD CONSTRAINT FK_1D5EF26F5278319C FOREIGN KEY (series_id) REFERENCES movie (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
